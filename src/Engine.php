75661a2b63000edb63a2537f17930a87f4752ba9
artem-povilaitis <artem.povilaitis@ya.ru>
2023-08-22 21:30:44 +0300

    fix

--- src/Engine.php
+++ src/Engine.php
@@ -11,7 +11,7 @@ const ROUNDS_COUNT = 3;
 
 function sayHello()
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
