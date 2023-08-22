f8c72b220f11066ea489109f603a5e87be416c84
artem-povilaitis <artem.povilaitis@ya.ru>
2023-08-22 22:04:39 +0300

    primeNumberList fix

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -9,7 +9,7 @@ use const BrainGames\Engine\ROUNDS_COUNT;
 
 function createPrimeNumbers()
 {
-    $primes = [2, 3, 4, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+    $primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
     return $primes;
 }
 
